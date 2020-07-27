dc534993a2f9f5a0a00856c0e8f1913e07183e5b
Nikita Yakunin <nayakunin@avito.ru>
2020-07-27 23:31:24 +0300

    Fix faq component

--- src/pages/HomePage/components/FAQ/components/Question/Question.jsx
+++ src/pages/HomePage/components/FAQ/components/Question/Question.jsx
@@ -1,33 +1,36 @@
-import React, { useRef, useCallback } from 'react';
+import React, { useRef, useCallback, useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { Typography } from '@material-ui/core';
+import { Typography, Grid } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
 
 const useStyles = makeStyles({
   root: {
-    cursor: 'pointer',
-    height: '50px',
-    overflow: 'hidden',
     position: 'relative',
     border: '1px solid red',
-    transition: 'all .3s',
+    padding: '10px',
+    transition: 'all .3s ease-in-out',
+    overflow: 'hidden',
+    cursor: 'pointer',
   },
-  active: {
-    height: 'fit-content',
-  },
-  title: {
-    paddingLeft: '80px',
-    lineHeight: '50px',
-    marginBottom: '12px',
+  top: {
+    marginTop: '8px',
+    marginBottom: '8px',
   },
   number: {
-    position: 'absolute',
     fontSize: '40px',
-    lineHeight: '40px',
     color: 'red',
-    opacity: '0.8',
-    top: '5px',
-    left: '5px',
+    opacity: '0.6',
+  },
+  dropdown: {
+    transition: 'all .3s ease-in-out',
+    overflow: 'hidden',
+  },
+  dropdown__icon: {
+    width: '40px',
+    height: '40px',
+    color: 'red',
+    transition: 'all .3s ease-in-out',
   },
 });
 
@@ -38,20 +41,53 @@ export const Question = ({
 }) => {
   const classes = useStyles();
   const itemRef = useRef();
+  const dropdownRef = useRef();
+  const [dropdownActive, setDropdownActive] = useState(true);
+  const [dropdownHeight, setDropdownHeight] = useState();
 
   const handleToggle = useCallback(() => {
-    itemRef.current.classList.toggle(classes.active);
-  });
+    setDropdownActive((prev) => !prev);
+  }, []);
+
+  useEffect(() => {
+    setDropdownHeight(window.getComputedStyle(dropdownRef.current).height);
+    setDropdownActive(false);
+  }, [dropdownRef]);
 
   return (
-    <div className={classes.root} ref={itemRef} onClick={handleToggle}>
-      <div className={classes.number}>{id}</div>
-      <Typography className={classes.title} variant="h6">
-        {title}
-      </Typography>
-      <Typography>
-        {children}
-      </Typography>
+    <div
+      className={classes.root}
+      ref={itemRef}
+      onClick={handleToggle}
+    >
+      <Grid container spacing={1} className={classes.top}>
+        <Grid item xs={1} container direction="column" justify="center" alignItems="center">
+          <span className={classes.number}>{id}</span>
+        </Grid>
+        <Grid item xs={10} container direction="column" justify="center">
+          <Typography variant="h6">
+            {title}
+          </Typography>
+        </Grid>
+        <Grid item xs={1} container direction="column" justify="center" alignItems="center">
+          <ArrowDropDownIcon
+            className={classes.dropdown__icon}
+            style={{ transform: dropdownActive ? 'rotate(180deg)' : 'rotate(0deg)' }}
+          />
+        </Grid>
+      </Grid>
+      <Grid
+        container
+        className={classes.dropdown}
+        ref={dropdownRef}
+        style={{ height: dropdownActive ? dropdownHeight : '0' }}
+      >
+        <Grid item xs={12}>
+          <Typography>
+            {children}
+          </Typography>
+        </Grid>
+      </Grid>
     </div>
   );
 };
