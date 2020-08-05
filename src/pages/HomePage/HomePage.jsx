5e0c767dc3361975460d37932bf7df477375e8bf
Nikita Yakunin <nayakunin@avito.ru>
2020-08-05 23:22:03 +0300

    Add section names

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect, useRef, useLayoutEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import { Header } from '../../components/Header/Header';
 import { Carousel } from './components/Carousel/Carousel';
 import { Description } from './components/Description/Description';
@@ -19,29 +20,80 @@ import { ShopImg } from './components/ShopImg/ShopImg';
 import { Map } from './components/Map/Map';
 import { Footer } from '../../components/Footer/Footer';
 
-// TODO Объединить в секции
-export const HomePage = () => (
-  <>
-    <Header />
-    <main>
-      <Carousel />
-      <Description />
-      <Catalog />
-      <DesignOffer />
-      <FeedbackForm />
-      <Advantages />
-      <WardrobeLeft />
-      <WardrobeRight />
-      <About />
-      <Order />
-      <CallDesignerForm />
-      <OurProduction />
-      <HowToOrder />
-      <FAQ />
-      <QuestionsForm />
-      <ShopImg />
-      <Map />
-    </main>
-    <Footer />
-  </>
-);
+export const HomePage = () => {
+  const refsMap = useRef({});
+  const location = useLocation();
+
+  useEffect(() => {
+    refsMap.current = {};
+  }, []);
+
+  useLayoutEffect(() => {
+    if (location.hash) {
+      refsMap.current[location.hash].scrollIntoView({
+        behavior: 'smooth',
+        block: 'center',
+      });
+    } else {
+      window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: 'smooth',
+      });
+    }
+  }, [refsMap.current, location.hash]);
+
+  return (
+    <>
+      <Header />
+      <main>
+        <section ref={(el) => { refsMap.current['#carousel'] = el; }}>
+          <Carousel />
+          <Description />
+        </section>
+        <section ref={(el) => { refsMap.current['#catalog'] = el; }}>
+          <Catalog />
+        </section>
+        <section ref={(el) => { refsMap.current['#design-offer'] = el; }}>
+          <DesignOffer />
+          <FeedbackForm />
+        </section>
+        <section ref={(el) => { refsMap.current['#advantages'] = el; }}>
+          <Advantages />
+        </section>
+        <section ref={(el) => { refsMap.current['#comfort'] = el; }}>
+          <WardrobeLeft />
+        </section>
+        <section ref={(el) => { refsMap.current['#quality'] = el; }}>
+          <WardrobeRight />
+        </section>
+        <section ref={(el) => { refsMap.current['#about'] = el; }}>
+          <About />
+        </section>
+        <section ref={(el) => { refsMap.current['#call-designer'] = el; }}>
+          <Order />
+          <CallDesignerForm />
+        </section>
+        <section ref={(el) => { refsMap.current['#production'] = el; }}>
+          <OurProduction />
+        </section>
+        <section ref={(el) => { refsMap.current['#how-to-order'] = el; }}>
+          <HowToOrder />
+        </section>
+        <section ref={(el) => { refsMap.current['#faq'] = el; }}>
+          <FAQ />
+        </section>
+        <section ref={(el) => { refsMap.current['#questions'] = el; }}>
+          <QuestionsForm />
+        </section>
+        <section ref={(el) => { refsMap.current['#shop-img'] = el; }}>
+          <ShopImg />
+        </section>
+        <section ref={(el) => { refsMap.current['#contacts'] = el; }}>
+          <Map />
+        </section>
+      </main>
+      <Footer />
+    </>
+  );
+};
