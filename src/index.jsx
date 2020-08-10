6cfbce7d847a4aa2732bb3b85e98c5ff28709544
Nikita Yakunin <nayakunin@avito.ru>
2020-08-10 21:46:17 +0300

    Add theme provider

--- src/index.jsx
+++ src/index.jsx
@@ -3,15 +3,23 @@ import ReactDOM from 'react-dom';
 import {
   BrowserRouter as Router,
 } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { ThemeProvider } from '@material-ui/core/styles';
+import { configureStore } from './store';
 import App from './App';
+import { theme } from './styles';
 import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* TODO Find a solution */}
-    <Router basename={process.env.PUBLIC_URL}>
-      <App />
-    </Router>
+    <Provider store={configureStore()}>
+      {/* TODO Find a solution */}
+      <Router basename={process.env.PUBLIC_URL}>
+        <ThemeProvider theme={theme}>
+          <App />
+        </ThemeProvider>
+      </Router>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
