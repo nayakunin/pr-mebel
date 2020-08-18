e89761d898c64bdcf2eb4ad93435de798a040ad7
Nikita Yakunin <nayakunin@avito.ru>
2020-08-18 23:47:36 +0300

    Added fonts

--- src/styles/index.js
+++ src/styles/index.js
@@ -6,4 +6,38 @@ export const theme = createMuiTheme({
       main: '#ff001f',
     },
   },
+  overrides: {
+    MuiTypography: {
+      h1: {
+        fontFamily: 'Gilroy',
+      },
+      h2: {
+        fontFamily: 'Gilroy',
+      },
+      h3: {
+        fontFamily: 'Gilroy',
+      },
+      h4: {
+        fontFamily: 'Gilroy',
+      },
+      h5: {
+        fontFamily: 'Gilroy',
+      },
+      h6: {
+        fontFamily: 'Gilroy',
+      },
+      subtitle1: {
+        fontFamily: 'Gilroy',
+      },
+      subtitle2: {
+        fontFamily: 'Gilroy',
+      },
+      body1: {
+        fontFamily: 'Gilroy',
+      },
+      body2: {
+        fontFamily: 'Gilroy',
+      },
+    },
+  },
 });
